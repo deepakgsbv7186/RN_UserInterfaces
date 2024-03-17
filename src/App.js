224cea5336ccf1e925ddbf68868ecae9a2a742cb
deepak.verma <deekgsbv@gmail.com>
2024-03-17 14:36:43 +0530

    add fonts poppins, lato, russo one and racingsansone

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import {View, Text, StyleSheet} from 'react-native';
 import React from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {FONT} from './assets/fonts/index';
+import {COLOR} from './utils/theme/colors';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Icon name="rocket" size={60} color="#900" />
+      <Text style={styles.tileText}>Practice UI Daily</Text>
     </View>
   );
 }
@@ -15,5 +18,10 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  tileText: {
+    fontFamily: FONT.Lato400,
+    fontSize: 20,
+    color: COLOR.approved,
   },
 });
